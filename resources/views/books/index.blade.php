101e5fdf63865e6500fb20d07b981b9eeeadbf82
Aki <103581174+Akii-Dev@users.noreply.github.com>
2025-01-09 09:49:29 +0100

    index books button styling

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,9 +3,22 @@
     <p class="text-center font-bold text-gray-600 mb-4">All books</p>
 
     @foreach ($books as $book)
-    <div class="bg-white shadow-lg m-2 p-2 text-sm rounded-md">
-        <p class="text-gray-800 font-bold">{{$book->title}}</p>
-        <p class="text-gray-500 font-semibold">{{$book->author}}</p>
-    </div>
+        <div class="bg-white shadow-lg m-2 p-2 text-sm rounded-md">
+            <p class="text-gray-800 font-bold">{{ $book->title }}</p>
+            <div class="flex justify-between">
+                <p class="text-gray-500 font-semibold">{{ $book->author }}</p>
+                <div class="flex">
+                    @if ($book['read_at'] == 0)
+                    <p class="mr-2">Finished</p>
+                    <input type="checkbox" class="accent-orange-700" id="{{ $book['id'] }}" checked>
+                    @else
+                    <p class="mr-2">On the shelf</p>
+                    <input type="checkbox" class="accent-orange-700" id="{{ $book['id'] }}">
+                    @endif
+                </div>
+            </div>
+        </div>
+
+        
     @endforeach
 @endsection
